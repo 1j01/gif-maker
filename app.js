4d23f390f5f248a47e71872be1c83797a747afa6
Isaiah Odhner <isaiahodhner@gmail.com>
2014-01-20 03:23:51 -0500

    Attempt to size images based on their natural size, not the max size I gave them in css!

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ $(function(){
 	})();
 	
 	var gif;
+	var clearIt = false;
 	
 	var delay = $delay.val();
 	$delay.on('change',function(){
@@ -32,6 +33,7 @@ $(function(){
 			workers: 2,
 			quality: 10
 		});
+		clearIt = true;
 	};
 	newGif();
 	
@@ -62,6 +64,8 @@ $(function(){
 			$done.addClass('failed').text('Failed');
 			$error.text(e.message);
 			console.error(e);
+		}finally{
+			newGif();
 		}
 	});
 	
@@ -74,6 +78,14 @@ $(function(){
 	$('body')
 		.on('dragover dragenter',prevent)
 		.on('drop',function(e){prevent(e);
+			if(clearIt){
+				var $oldFrames = $(".frame");
+				$oldFrames.addClass("poof");
+				setTimeout(function(){
+					//$oldFrames.remove();
+				},1000);
+				clearIt = false;
+			}
 			var dt = e.originalEvent.dataTransfer;
 			var files;
 			if(dt && (files=dt.files) && files.length){
@@ -90,6 +102,8 @@ $(function(){
 						$img.attr('src',reader.result);
 						$img.on('load',function(e){
 							var img = $img[0];
+							gif.options.width = img.naturalWidth;
+							gif.options.height = img.naturalHeight;
 							gif.addFrame(img, {delay: delay});
 							icon(function(ctx){
 								ctx.drawImage(img,0,0,16,16);
