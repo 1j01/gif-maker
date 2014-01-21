529003de7b45f030092c42b44e6e793e65352ba2
Isaiah Odhner <isaiahodhner@gmail.com>
2014-01-20 20:25:47 -0500

    Better looks, more error handling.

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ $(function(){
 			return;
 		}
 		if(!gif.frames.length){
-			$error.text('Add some images first!');
+			if($('.frame').length == 0){
+				$error.text('Add some images first!');
+			}else{
+				$error.text("Due to previous failure, I had to start over. You'll have to re-add those frames to try again.");
+			}
 			return;
 		}
 		gif.on('finished', function(blob){
@@ -78,43 +82,57 @@ $(function(){
 	$('body')
 		.on('dragover dragenter',prevent)
 		.on('drop',function(e){prevent(e);
-			if(clearIt){
-				var $oldFrames = $(".frame");
-				$oldFrames.addClass("poof");
-				setTimeout(function(){
-					$oldFrames.remove();
-				},1200);
-				clearIt = false;
-			}
+			$error.text('');
 			var dt = e.originalEvent.dataTransfer, files;
 			if(dt && (files=dt.files) && files.length){
+				if(clearIt){
+					var $oldFrames = $(".frame");
+					$oldFrames.addClass("poof");
+					setTimeout(function(){
+						$oldFrames.remove();
+					},1200);
+					clearIt = false;
+				}
 				//console.debug("dropped files: ",files);
-				var i=0;
+				var i = 0;
+				var n_images = 0;
+				var n_other = 0;
 				var next = function(){
 					var file = files[i++];
-					if(!file){return;}
+					if(!file){
+						if(n_other && !n_images){
+							$error.text(n_other>1?"Those aren't images!":"That's not an image!");
+						}
+						return;
+					}
 					//console.debug(i,'out of',files.length,':',file);
 					
-					var reader = new FileReader();
-					reader.onload = function(e){
-						var $img = $('<img class="frame animated flipInY"/>').appendTo($frames);
-						$img.attr('src',reader.result);
-						$img.on('load',function(e){
-							var img = $img[0];
-							gif.options.width = img.naturalWidth;
-							gif.options.height = img.naturalHeight;
-							gif.addFrame(img, {delay: delay});
-							icon(function(ctx){
-								ctx.drawImage(img,0,0,16,16);
+					if(file.type.match(/image/)){
+						n_images++;
+						var reader = new FileReader();
+						reader.onload = function(e){
+							var $img = $('<img class="frame animated flipInY"/>').appendTo($frames);
+							$img.attr('src',reader.result);
+							$img.on('load',function(e){
+								var img = $img[0];
+								gif.options.width = img.naturalWidth;
+								gif.options.height = img.naturalHeight;
+								gif.addFrame(img, {delay: delay});
+								icon(function(ctx){
+									ctx.drawImage(img,0,0,16,16);
+								});
+								next();
 							});
-							next();
-						});
-					};
-					reader.onerror = function(e){
-						$error.text(e.message);
-						console.error(reader.result,e);
-					};
-					reader.readAsDataURL(file);
+						};
+						reader.onerror = function(e){
+							$error.text(e.message);
+							console.error(reader.result,e);
+						};
+						reader.readAsDataURL(file);
+					}else{
+						n_other++;
+						next();
+					}
 				};
 				next();
 			}
